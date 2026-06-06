334e8e2dcdceb5d340edead59a2892b082337410
Анатолий <TOLRH333@gmail.com>
2026-06-06 17:10:43 +1000

    Completed create Domain models
    Created Domain models:
    1. Question
    2. Answer
    3. Tag
    4. Report
    5. Status

---
+++ QuestionsAPI/src/QuestionsAPI.Domain/Tags/Tag.cs
@@ -0,0 +1,8 @@
+﻿namespace QuestionsAPI.Domain.Tags;
+
+public class Tag
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Description { get; set; }
+}
